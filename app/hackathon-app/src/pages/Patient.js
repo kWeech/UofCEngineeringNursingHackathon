39aff69ae555f3d40cb09e1d2243cfcf898e6553
IWwilliams <isaiah.williams@ucalgary.ca>
2023-03-04 14:49:37 -0700

    fixing

--- app/hackathon-app/src/pages/Patient.js
+++ app/hackathon-app/src/pages/Patient.js
@@ -19,7 +19,7 @@ const modalHandlerController = (state, action) => {
         modalType: <Medication content={state.patient.medication}></Medication>,
       };
     }
-    case "Call/Text": {
+    case "Contact Information": {
       console.log("entered call/text");
       return {
         ...state,
