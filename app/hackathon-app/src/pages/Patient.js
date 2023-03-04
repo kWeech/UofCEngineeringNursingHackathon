9a2896ee52efbd4dfb138fd73cb546a11dab02ba
KBaldwin2 <kimberley.baldwin@ucalgary.ca>
2023-03-04 12:55:11 -0700

    working on patient

--- app/hackathon-app/src/pages/Patient.js
+++ app/hackathon-app/src/pages/Patient.js
@@ -6,18 +6,17 @@ import { useLocation } from "react-router-dom";
 
 export default function Patient(props) {
   const location = useLocation();
-  console.log(props.location);
   return (
     <>
       <h1>Patient Menu</h1>
-      <Modal></Modal>
+      <Modal>
       <Button>Call/Text</Button>
       <Button>Medications</Button>
       <Button>Care Team List</Button>
       <NavLink to="/flowsheet">
         <Button>FLOWSHEETS</Button>
       </NavLink>
-      <Button>Community Appointments</Button>
+      <Button>Community Appointments</Button></Modal>
     </>
   );
 }
