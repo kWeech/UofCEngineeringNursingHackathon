213d82ab7f2f0c487218253f639aa7e51a3b05db
Your Name <kenneth.weech@gmail.com>
2023-03-04 11:38:50 -0700

    added scanner

--- app/hackathon-app/src/pages/QR.js
+++ app/hackathon-app/src/pages/QR.js
@@ -1,9 +1,27 @@
-import Page from '../components/Page';
-export default function QR() {
-<<<<<<< HEAD
-  return <h1>This should just open up camera??</h1>;
+import React from "react";
+import QrReader from "react-qr-reader";
+
+function QR(props) {
+  const handleScan = (data) => {
+    if (data) {
+      props.onScan(data);
+    }
+  };
+
+  const handleError = (err) => {
+    console.error(err);
+  };
+
+  return (
+    <div>
+      <QrReader
+        delay={300}
+        onError={handleError}
+        onScan={handleScan}
+        style={{ maxHeight: "100%", maxWidth: "100%" }}
+      />
+    </div>
+  );
 }
-=======
-    return<Page>This should just open up camera??</Page>
-}
->>>>>>> 0480af4def03668e4faae8e0fce6656f8e1e65f6
+
+export default QR;
