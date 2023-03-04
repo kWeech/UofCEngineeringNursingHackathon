5e2e4ce873420a310c5094e6b62e3270e974f858
Your Name <kenneth.weech@gmail.com>
2023-03-04 12:43:30 -0700

    fixed page wrappers

--- app/hackathon-app/src/pages/Policy.js
+++ app/hackathon-app/src/pages/Policy.js
@@ -1,3 +1,9 @@
+import Page from "../components/Page";
+
 export default function Policy() {
-    return <h1>HERE ARE SOME POLICIES</h1>
+  return (
+    <Page>
+      <h1>HERE ARE SOME POLICIES</h1>
+    </Page>
+  );
 }
