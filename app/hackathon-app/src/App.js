4465857bc9c7467baf93f50f1217ac100ee692a3
Your Name <kenneth.weech@gmail.com>
2023-03-04 09:09:41 -0700

    inital commit

---
+++ app/hackathon-app/src/App.js
@@ -0,0 +1,12 @@
+import logo from "./logo.svg";
+import classes from "./App.module.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header"></header>
+    </div>
+  );
+}
+
+export default App;
