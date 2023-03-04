48d3aeb97923cafa81f73bfac80dc807726eda08
KBaldwin2 <kimberley.baldwin@ucalgary.ca>
2023-03-04 11:51:35 -0700

    Saving notes to notes array

--- app/hackathon-app/src/pages/NurseNotes.js
+++ app/hackathon-app/src/pages/NurseNotes.js
@@ -1,15 +1,28 @@
 import { useState } from 'react';
 import { useSpeechRecognition } from 'react-speech-kit';
+import Note from '../components/Note';
+
+const NOTES = [
+  <Note date={"2022-12-03"} note={"This is a note"}/>,
+  <Note date={"2022-12-15"} note={"This patient was so bad omg"}/>,
+  <Note date={"2022-12-03"} note={"This was a great patient love them xoxo"}/>,
+ ]
 
 export default function NurseNotes() {
-  const [notesArray, setNotesArray] = useState(dummyNotes);
+  const [notesArray, setNotesArray] = useState(NOTES);
  const [value, setValue] = useState('')
  const { listen, stop } = useSpeechRecognition({
    onResult: (result) => {
      setValue(result)
    }
  })
-  
+
+const onSave = () => {
+  const newNote = (<Note date={"2023-03-04"} note={value}/>)
+  NOTES.push(newNote)
+  setNotesArray(newNote);
+}
+
  return (
    <div>
      <textarea
@@ -22,7 +35,9 @@ export default function NurseNotes() {
       <button onMouseDown={stop}>
         🎤 Stop
       </button>
-      <p>Yo check me out im text to speech</p>
+      <button onClick={onSave}>Save</button>
+      <h2>Historic Notes</h2>
+      <div>{NOTES}</div>
      </div>
     )
   }
