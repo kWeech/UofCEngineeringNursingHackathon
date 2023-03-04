7aaa84ee15ea0236c193adb94df9f64a49c3aed2
KBaldwin2 <kimberley.baldwin@ucalgary.ca>
2023-03-04 14:47:45 -0700

    added flowsheet

--- app/hackathon-app/src/pages/Flowsheets.js
+++ app/hackathon-app/src/pages/Flowsheets.js
@@ -5,7 +5,7 @@ export default function Flowsheets() {
     return(
         <Page>
             <h1>Patient Flowsheet</h1>
-            <p>Click <a href="/src/pdfs/FlowSheet_ADMH.pdf" target="_blank">here</a> to view the Flow Sheet PDF file.</p>
+            <p>Click <a href="http://cirrie-sphhp.webapps.buffalo.edu/culture/simulations/forms/2/nursing.pdf" target="_blank">here</a> to view the Flow Sheet PDF file.</p>
         </Page>
         );
 }
