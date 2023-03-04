1a54a6c3e1513ff5fb23de3db9a306fe0dd2d9f4
Your Name <kenneth.weech@gmail.com>
2023-03-04 11:49:40 -0700

    changed button and hompage

--- app/hackathon-app/src/pages/Home.js
+++ app/hackathon-app/src/pages/Home.js
@@ -1,32 +1,35 @@
 import { NavLink } from "react-router-dom";
-import Page from '../components/Page';
-import Button from '../components/Button';
+import Page from "../components/Page";
+import Button from "../components/Button";
+import classes from "./Home.module.css";
 
 export default function Home() {
   return (
     <Page>
       <h1>THIS IS OUR HOME PAGE</h1>
-      <NavLink to="/flowsheet">
-        <Button>FLOWSHEETS</Button>
-      </NavLink>
-      <NavLink to="/nursenotes">
-        <Button>NURSENOTES</Button>
-      </NavLink>
-      <NavLink to="/qr">
-        <Button>QR</Button>
-      </NavLink>
-      <NavLink to="/patientlist">
-        <Button>PATIENTLIST</Button>
-      </NavLink>
-      <NavLink to="/policy">
-        <Button>POLICY</Button>
-      </NavLink>
+      <div className={classes.buttonContainer}>
+        <NavLink to="/flowsheet">
+          <Button>FLOWSHEETS</Button>
+        </NavLink>
+        <NavLink to="/nursenotes">
+          <Button>NURSENOTES</Button>
+        </NavLink>
+        <NavLink to="/qr">
+          <Button>QR</Button>
+        </NavLink>
+        <NavLink to="/patientlist">
+          <Button>PATIENTLIST</Button>
+        </NavLink>
+        <NavLink to="/policy">
+          <Button>POLICY</Button>
+        </NavLink>
         <NavLink to="/lexicomp">
-        <Button>LEXICOMP</Button>
-      </NavLink>
-      <NavLink to="/orders">
-        <Button>ORDERS</Button>
-      </NavLink>
+          <Button>LEXICOMP</Button>
+        </NavLink>
+        <NavLink to="/orders">
+          <Button>ORDERS</Button>
+        </NavLink>
+      </div>
     </Page>
   );
 }
--- app/hackathon-app/src/pages/Login.js
+++ app/hackathon-app/src/pages/Login.js
@@ -1,32 +1,32 @@
 import { NavLink } from "react-router-dom";
-import Page from '../components/Page';
-import { useState } from 'react';
+import Page from "../components/Page";
+import { useState } from "react";
 
 export default function Login() {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
 
   const isLoginFormValid = () => {
-    return username === 'Sara' && password === 'swordfish';
-  }
+    return username === "Sara" && password === "swordfish";
+  };
   return (
     <Page>
       <h1>LOGIN PAGE</h1>
       <NavLink to="/home">
-        <button disabled = {!isLoginFormValid()}>LOGIN</button>
+        <button disabled={!isLoginFormValid()}>LOGIN</button>
       </NavLink>
-      <form >
-        <input 
-          type = "text" 
-          placeholder = "Username"
-          value = {username}
-          onChange = {(event) => setUsername(event.target.value)}
+      <form>
+        <input
+          type="text"
+          placeholder="Username"
+          value={username}
+          onChange={(event) => setUsername(event.target.value)}
         />
         <input
-          type = "password"
-          placeholder = "Password"
-          value = {password}
-          onChange = {(event) => setPassword(event.target.value)}
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(event) => setPassword(event.target.value)}
         />
       </form>
     </Page>
