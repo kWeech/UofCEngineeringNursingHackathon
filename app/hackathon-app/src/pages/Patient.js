f4923656ea492fc4e95ae2cc26f6136227ff88bb
Braydon <hartbraydon@gmailcom>
2023-03-04 12:52:27 -0700

    Added a modal

--- app/hackathon-app/src/pages/Patient.js
+++ app/hackathon-app/src/pages/Patient.js
@@ -5,19 +5,30 @@ import Modal from "../components/Modal";
 import { useLocation } from "react-router-dom";
 
 export default function Patient(props) {
-  let location = useLocation();
-  console.log(location);
-  return (
-    <>
-      <Modal></Modal>
-      <h1>Patient Menu</h1>
-      <Button>Call/Text</Button>
-      <Button>Medications</Button>
-      <Button>Care Team List</Button>
-      <NavLink to="/flowsheet">
-        <Button>FLOWSHEETS</Button>
-      </NavLink>
-      <Button>Community Appointments</Button>
-    </>
-  );
+    const [showModal, setShowModal] = useState(false);
+    const handleCallTextButtonClick = () => {
+        setShowModal(true);
+      };
+    
+      const handleCloseModal = () => {
+        setShowModal(false);
+      };
+    return (
+        <Page>
+          <h1>Patient Menu</h1> 
+          <Button onClick={handleCallTextButtonClick}>Call/Text</Button>
+          <Button>Medications</Button>
+          <Button>Care Team List</Button>
+          <NavLink to="/flowsheet">
+            <Button>FLOWSHEETS</Button>
+          </NavLink>
+          <Button>Community Appointments</Button>
+          {showModal && (
+            <Modal onClose={handleCloseModal}>
+              <h2>Call/Text Modal</h2>
+              <p>Phone number: 403-999-9999</p>
+            </Modal>
+      )}
+        </Page>
+    );
 }
